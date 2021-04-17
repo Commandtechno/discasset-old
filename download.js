7347c32dbe43f12c0f5fc8bd799f3e5c80daad04
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2021-04-16 23:07:26 -0500

    derpy say new update so i update

--- download.js
+++ download.js
@@ -22,56 +22,61 @@ const buildRegex = /GLOBAL_ENV\.RELEASE_CHANNEL,.=null==="(.{40})/g //Regex to g
 const buildIdRegex = /,"([1234567890]{5})"/g //Regex to get the build id
 const canary = 'https://canary.discord.com/login' //Entrypoint to get all the app data from
 const assetHost = 'https://canary.discord.com' //Host for assets
+const environment = "canary"
 
-async function download() {
-  console.log('[Download] Starting...')
+async function download(){
   const storage = fs.readFileSync('data.json') // Read and parse jsonstorage
-  var jsonstorage = JSON.parse(storage)
+  var jsonstorage = JSON.parse(storage) 
   let site;
-  try {
-    site = await superagent.get(canary) //Fetch login page so we can extract scripts and stylesheet
-    console.log('[Download] Client Data Fetched. Searching for main script...')
-    const match = site.text.match(script) //Get all scripts from login page
-    scriptData = await superagent.get(`${assetHost}${match[0].replace(`script src="`, '')}`) //Load master script data, then extract json
-    scripts = loaderRegex.exec(scriptData.body) //Extract the required JSON from the response script
-    scriptData = scripts[1].toString().replace(jsonFix, "\"$1\":") //Fix the JSON so we can parse it
-    scripts = JSON.parse(scriptData) //Parse the data
+  try{
+    //check version
+    ver = await superagent.get(`${assetHost}/assets/version.${environment}.json`)
+    if(jsonstorage.version.buildHash == ver.body.hash){
+      console.log(`[Download] Version ${jsonstorage.version.buildId} (${jsonstorage.version.buildHash}) is already downloaded. Press any key to exit.`)
+      process.stdin.setRawMode(true);
+      process.stdin.resume();
+      process.stdin.on('data', process.exit.bind(process, 0));
+    }
+    site = await superagent.get(canary)
+    const match = site.text.match(script) 
+    scriptData = await superagent.get(`${assetHost}${match[0].replace(`script src="`, '')}`)
+    scripts = loaderRegex.exec(scriptData.body)
+    scriptData = scripts[1].toString().replace(jsonFix, "\"$1\":")
+    scripts = JSON.parse(scriptData) 
     //The client script
     clientScript = await superagent.get(`${assetHost}${match[3].replace(`script src="`, '')}`)
-    fs.writeFileSync('data.client.txt', clientScript.body, 'utf8') //Save Client
+    fs.writeFileSync('data.client.txt', clientScript.body, 'utf8')
     let mainScript = ""
-    Object.values(scripts).forEach(async function (scr) { //Try to locate the correct script
-      if (mainScript !== "") {
-        return; //we already have the script and dont need to do any more searching
+    Object.values(scripts).forEach(async function(scr){
+      if(mainScript !== ""){
+        return;
       }
       data = await superagent.get(`${assetHost}/assets/${scr}.js`)
-      const buildData = buildRegex.exec(data.body) //Extract the build hash, if this works we have the main script
-      const buildId = buildIdRegex.exec(data.body) //Extract the build ID
-      if (buildData) {
-        mainScript = data.body //This is the main JS file we want/need
-        if (jsonstorage.version.buildHash == buildData[1]) { //Check if we already have the latest version
-          //We already have the current version, display a message and prompt an exit
-          console.log(`[Download] Version ${jsonstorage.version.buildId} (${jsonstorage.version.buildHash}) is already downloaded.`)
-          process.exit()
+      const buildData = buildRegex.exec(data.body)
+      const buildId = buildIdRegex.exec(data.body)
+      if(buildData){
+        mainScript = data.body
+        if(jsonstorage.version.buildHash == buildData[1]){
         } else {
           jsonstorage.version.buildHash = buildData[1]
           jsonstorage.version.buildId = buildId[1]
-          fs.writeFileSync('data.json', JSON.stringify(jsonstorage, null, 2), 'utf8') //Store version
+          fs.writeFileSync('data.json', JSON.stringify(jsonstorage, null, 2), 'utf8')
           console.log(`[Download] Downloading files for build ${jsonstorage.version.buildId} (${jsonstorage.version.buildHash.substr(0, 7)})...`)
-          fs.writeFileSync('data.js.txt', mainScript, 'utf8') //Save main script
+          fs.writeFileSync('data.js.txt', mainScript, 'utf8')
           console.log('[Download] JavaScript Downloaded.')
           //Stylesheet Download
-          const matches = stylesheet.exec(site.text) //Extract stylesheet location
-          style = await superagent.get(`${assetHost}${matches[1]}`) //Download Stylesheet
-          fs.writeFileSync('data.css.txt', style.text, 'utf8') //Save Stylesheet
+          const matches = stylesheet.exec(site.text)
+          style = await superagent.get(`${assetHost}${matches[1]}`)
+          fs.writeFileSync('data.css.txt', style.text, 'utf8')
           console.log('[Download] Stylesheet Downloaded.')
-          //Download should be finished, print a message
-          console.log(`[Download] Successfully downloaded files for build ${jsonstorage.version.buildId} (${jsonstorage.version.buildHash}).`)
-          process.exit()
+          console.log(`[Download] Successfully downloaded files for build ${jsonstorage.version.buildId} (${jsonstorage.version.buildHash}). Press any key to exit.`)
+          process.stdin.setRawMode(true);
+          process.stdin.resume();
+          process.stdin.on('data', process.exit.bind(process, 0));
         }
       }
     })
-  } catch (e) {
+  } catch(e){
     console.log('[Error] Failed to download discord data:')
     console.log(e)
   }
